99659a69c2735a24a141db9eae1ab6c56a7b604a
Diwakar Shukla <diwakar@redorangetechnologies.com>
2018-02-28 11:23:31 +0530

    service in index

--- src/common/services/index.ts
+++ src/common/services/index.ts
@@ -1,5 +1,6 @@
 import AuthService from './AuthService';
 import * as ValidationService from './ValidationService';
 import ProfileService from "./ProfileService";
+import FamilyMemberService from "./FamilyService";
 
-export { AuthService, ValidationService, ProfileService };
+export { AuthService, ValidationService, ProfileService, FamilyMemberService };
