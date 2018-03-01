9e6bd7a4daddfcd948ddbd828b7551348333c968
revan <revansiddh.patil@roxiler.com>
2018-03-01 17:24:51 +0530

    date picker formated and shown selected date

--- src/common/components/fieldComponents/datePicker.tsx
+++ src/common/components/fieldComponents/datePicker.tsx
@@ -10,7 +10,7 @@ class FormDatePicker extends Component {
         const {input, label, placeholder, type, required, meta: { touched, error, warning }} = this.props;
         const hasError = touched && error;
         const errorComponent = hasError ? <Text style={{marginLeft: 16, color: Colors.colorDanger}}>{error}</Text> : null;
-        const selectedDate = input.value ? input.value :"Select DOB";
+        const selectedDate = input.value ? moment(input.value).format("DD/MM/YY") :"Select DOB";
 
         return (
             <Item stackedLabel>
@@ -27,12 +27,13 @@ class FormDatePicker extends Component {
             const datemin = moment().toDate();  
             const {action, year, month, day} = await DatePickerAndroid.open({ 
                 maxDate:datemin,
+                date:moment(this.props.input.value).toDate(),
                 mode : 'spinner'
             });
             if(action !== DatePickerAndroid.dismissedAction) {
                 const dob = `${day}/${month+1}/${year}`
                 this.setState({selectedDob:dob});
-                this.props.input.onChange(dob);
+                this.props.input.onChange(moment(dob).format("YYYY/MM/DD"));
             }
         } catch (error) {
             console.log(error)
