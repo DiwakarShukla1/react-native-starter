7d57c251f5719a26f3f78b50ceb3883e3aab5572
Diwakar Shukla <diwakar@redorangetechnologies.com>
2018-03-01 18:20:54 +0530

    upload file service

--- src/common/services/UploadService.ts
+++ src/common/services/UploadService.ts
@@ -1,21 +1,66 @@
-import { RNS3 } from 'react-native-aws3';
+// import Config from '../config';
+// import HttpService from "../services/HttpService";
 
-const options = {
-  keyPrefix: "photos/",
-  bucket: "farmers.files",
-  region: "us-east-1",
-  accessKey: "AKIAJ5DH7L4B2YAYPX3A",
-  secretKey: "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",
-  successActionStatus: 201
-}
+/* tslint:disable */
+// const Window: any = window;
 
-const obj = {
-  async uploadFile (file: any) {
-    const response = await RNS3.put(file, options);
-    if (response.status !== 201)
-      throw new Error("Failed to upload image to S3"); 
-    console.log(response.body);  
+// tslint:disable-next-line:typedef
+const API_SERVER_URL = 'http://192.168.0.112:3001/';
+
+export function sign_and_upload(file, progressHandler, folder) {
+    return new Promise( (resolve, reject) => {
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET',  API_SERVER_URL + 'sign_s3?file_name=' + file.name + '&file_type=' + file.type + '&folder=' + folder);
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            var response = JSON.parse(xhr.responseText);
+            upload_file(file, response.signed_request, response.url, resolve, reject, progressHandler);
+          } else {
+            reject('Could not get signed URL');
+          }
+        }
+      };
+      xhr.send();
+    });
   }
+  
+function upload_file(file: any, signedRequest: any, url: any, resolve: any, reject: any,progressHandler: any) {
+    // console.log(signedRequest);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('PUT', signedRequest);
+    xhr.setRequestHeader('x-amz-acl', 'public-read');
+    xhr.setRequestHeader('mode', 'no-cors');
+
+    xhr.upload.onprogress = (evt) => {
+      if (evt.lengthComputable && progressHandler) {
+        progressHandler((evt.loaded / evt.total)*100);
+      }
+    };
+  
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        resolve(url);
+      }
+    };
+  
+    xhr.onerror = (err) => {
+      reject(err);
+    //   Window.error('Could not upload file. ' + err);
+    };
+  
+    xhr.send(file);
+    // HttpService.put()
+
+  }
+
+
+const uploadFile = async (file: any) => {
+  const url =  await sign_and_upload(file, (data) => {console.log(data);}, "photos");
+  return url;
 };
+
+const obj = { uploadFile };
 
 export default obj;
