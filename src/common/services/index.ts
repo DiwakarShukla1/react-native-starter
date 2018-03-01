0eefc5b2b87000bb3b20f96897327dd0db0083a6
Diwakar Shukla <diwakar@redorangetechnologies.com>
2018-03-01 10:48:59 +0530

    Upload Service

--- src/common/services/index.ts
+++ src/common/services/index.ts
@@ -3,5 +3,6 @@ import * as ValidationService from './ValidationService';
 import ProfileService from "./ProfileService";
 import FamilyMemberService from "./FamilyService";
 import FarmService from "./FarmService";
+import UploadService from "./UploadService";
 
-export { AuthService, FarmService, ValidationService, ProfileService, FamilyMemberService };
+export { AuthService, UploadService, FarmService, ValidationService, ProfileService, FamilyMemberService };
